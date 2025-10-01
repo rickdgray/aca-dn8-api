d12a6795e36ff060ca0dadb741881e1cc393f97c
Rick Gray <rickdgray@outlook.com>
2025-10-01 18:13:11 -0500

    logs

--- Program.cs
+++ Program.cs
@@ -7,6 +7,7 @@ app.Urls.Add("http://0.0.0.0:8080");
 app.MapGet("/", () =>
 {
     Console.WriteLine(">>> Log from inside the container!");
+    Console.WriteLine(">>> Logs, logs, and more logs!");
     return $"Hello World @ UTC: {DateTime.UtcNow}!";
 })
 .WithName("hello");
